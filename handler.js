9c0eaa91e269da3712c575d5bd008843a39c2ed0
Fajing <faddie_w@163.com>
2021-03-23 00:03:17 +0800

    koa
    koa

--- handler.js
+++ handler.js
@@ -1,18 +1,39 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const serverlessHttp = require('serverless-http');
+const Koa = require('koa');
+const Router = require('koa-router');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const app = new Koa();
+const router = new Router();
+
+// app.use(ctx => {
+//   ctx. = 'Hello World';
+// });
+
+router.get('/get',(ctx) => {
+  ctx.body = 'body';
+})
+
+app.use(router.routes());
+
+module.exports= {
+  // app,
+  hello : serverlessHttp(app),
 };
+// module.exports.hello = async (event) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
