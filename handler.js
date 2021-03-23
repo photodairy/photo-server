4a78aa7e89c38a9375d7aa797537593f8ff76791
wangfajing <faddie_w@163.com>
2021-03-23 15:27:49 +0800

    connect db

--- handler.js
+++ handler.js
@@ -3,37 +3,68 @@
 const serverlessHttp = require('serverless-http');
 const Koa = require('koa');
 const Router = require('koa-router');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 const router = new Router();
+const { Schema, model } = mongoose;
 
-// app.use(ctx => {
-//   ctx. = 'Hello World';
-// });
 
-router.get('/get',(ctx) => {
+mongoose.connect('mongodb+srv://fajing:wangfajing@zhihu.57z0a.azure.mongodb.net/zhihu?retryWrites=true&w=majority');
+
+const userSchema = new Schema({
+  username: { type: String} ,
+  pwd: { type: String}
+})
+const userModel = model('users', userSchema);
+
+
+
+router.get('/get', async (ctx) => {
+  // const user = new userModel({username:'fajing',password:'66666666'}).save();
+  ctx.body = (await userModel.find()).toString();
+  // ctx.body = ctx.header;
+})
+
+router.get('/insert', (ctx) => {
+  const user = new userModel({username:'fajing',pwd:'66666666'}).save();
+  ctx.body = ctx.header;
+})
+
+router.get('/test',(ctx) => {
   ctx.body = 'body';
 })
 
 app.use(router.routes());
 
+// or as a promise
+const apphandler = serverlessHttp(app);
+const api = async (event, context) => {
+  // you can do other things here
+  const result = await apphandler(event, context);
+  // and here
+  return result;
+};
+
+
+const test = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
 module.exports= {
   // app,
-  hello : serverlessHttp(app),
+  test: test,
+  api : api
 };
-// module.exports.hello = async (event) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Go Serverless v1.0! Your function executed successfully!',
-//         input: event,
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
