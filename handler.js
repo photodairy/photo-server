045a7b1f75c482b617b989b325e8fc676da36936
fajing <faddie_w@163.com>
2021-04-13 18:55:08 +0800

    test debuger

--- handler.js
+++ handler.js
@@ -31,76 +31,106 @@ const s3 = new AWS.S3();
 
 const uploadFile = async (event, context, callback) => {
 
-    // Read options from the event parameter.
-    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-    // const srcBucket = event.Records[0].s3.bucket.name;
-    // Object key may have spaces or unicode non-ASCII characters.
-    // const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    // const dstBucket = srcBucket + "-resized";
-    // const dstKey    = "resized-" + srcKey;
+  // Read options from the event parameter.
+  // console.log("Reading options from event:\n", util.inspect(event, { depth: 5 }));
+  // const srcBucket = event.Records[0].s3.bucket.name;
+  // Object key may have spaces or unicode non-ASCII characters.
+  // const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  // const dstBucket = srcBucket + "-resized";
+  // const dstKey    = "resized-" + srcKey;
 
-    // Infer the image type from the file suffix.
-    // const typeMatch = srcKey.match(/\.([^.]*)$/);
-    // if (!typeMatch) {
-    //     console.log("Could not determine the image type.");
-    //     return;
-    // }
+  // Infer the image type from the file suffix.
+  // const typeMatch = srcKey.match(/\.([^.]*)$/);
+  // if (!typeMatch) {
+  //     console.log("Could not determine the image type.");
+  //     return;
+  // }
 
-    // Check that the image type is supported  
-    // const imageType = typeMatch[1].toLowerCase();
-    // if (imageType != "jpg" && imageType != "png") {
-    //     console.log(`Unsupported image type: ${imageType}`);
-    //     return;
-    // }
+  // Check that the image type is supported  
+  // const imageType = typeMatch[1].toLowerCase();
+  // if (imageType != "jpg" && imageType != "png") {
+  //     console.log(`Unsupported image type: ${imageType}`);
+  //     return;
+  // }
 
-    // Download the image from the S3 source bucket. 
-    // try {
-    //     const params = {
-    //         Bucket: srcBucket,
-    //         Key: srcKey
-    //     };
-    //     var origimage = await s3.getObject(params).promise();
+  // Download the image from the S3 source bucket. 
+  // try {
+  //     const params = {
+  //         Bucket: srcBucket,
+  //         Key: srcKey
+  //     };
+  //     var origimage = await s3.getObject(params).promise();
 
-    // } catch (error) {
-    //     console.log(error);
-    //     return;
-    // }  
+  // } catch (error) {
+  //     console.log(error);
+  //     return;
+  // }  
 
-    // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
-    // const width  = 200;
+  // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
+  // const width  = 200;
 
-    // Use the Sharp module to resize the image and save in a buffer.
-    // try { 
-    //     var buffer = await sharp(origimage.Body).resize(width).toBuffer();
-            
-    // } catch (error) {
-    //     console.log(error);
-    //     return;
-    // } 
+  // Use the Sharp module to resize the image and save in a buffer.
+  // try { 
+  //     var buffer = await sharp(origimage.Body).resize(width).toBuffer();
 
-    // Upload the thumbnail image to the destination bucket
-    try {
-        const destparams = {
-            // Bucket: dstBucket,
-            // Key: dstKey,
-            // Body: buffer,
-            // ContentType: "image"
+  // } catch (error) {
+  //     console.log(error);
+  //     return;
+  // } 
 
-            Bucket: srcBucket,
-            Key: srcKey,
-            Body: buffer,
-            ContentType: "image"
-        };
+  // Upload the thumbnail image to the destination bucket
+  const srcBucket = 'normal-photo';
+  const srcKey = 'Screenshot 2021-03-01 134825.png';
+  const buffer = event.body;
 
-        const putResult = await s3.putObject(destparams).promise(); 
-        
-    } catch (error) {
-        console.log(error);
-        return;
-    } 
-        
-    // console.log('Successfully resized ' + srcBucket + '/' + srcKey +
-    //     ' and uploaded to ' + dstBucket + '/' + dstKey); 
+  // try {
+  //   const destparams = {
+  //     // Bucket: dstBucket,
+  //     // Key: dstKey,
+  //     // Body: buffer,
+  //     // ContentType: "image"
+
+  //     Bucket: srcBucket,
+  //     Key: srcKey,
+  //     Body: buffer,
+  //     ContentType: "image"
+  //   };
+
+  //   const putResult = await s3.putObject(destparams).promise();
+
+  // } catch (error) {
+  //   console.log(error);
+  //   return;
+  // }
+  // console.log('Successfully upload file to ' + srcBucket + '/' + srcKey);
+
+  // Download the image from the S3 source bucket. 
+  try {
+    const params = {
+      Bucket: srcBucket,
+      Key: srcKey
+    };
+    var origimage = await s3.getObject(params).promise();
+
+    return {
+      statusCode: 200,
+      ContentType : 'image/png',
+      body: JSON.stringify(
+        {
+          file: origimage.Body,
+        },
+        null,
+        2
+      )
+    }
+    // console.log(context );
+    // console.log('Successfully download ' + srcBucket + '/' + srcKey );
+
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
 };
 
 const test = async (event) => {
@@ -119,9 +149,9 @@ const test = async (event) => {
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
-module.exports= {
+module.exports = {
   app,
   test: test,
   // api : api,
-  uploadFile : uploadFile
+  uploadFile: uploadFile
 };
